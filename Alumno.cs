74b85deae9dbfc2a992c6c306ab7787cfa8919c0
mmedina16 <miguel_medi@yahoo.com>
2024-11-07 10:02:55 -0600

    Add project files.

---
+++ Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Alumno
+    {
+        string NombreAlumno { get; set; }
+        string NumeroCuenta { get; set; }
+        string Email { get; set; }
+
+    }
+}
---
+++ Asignatura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Asignatura : Alumno
+    {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+        public int N3 { get; set; }
+
+        public string NombreAsignatura { get; set; }
+        public string Horario { get; set; }
+
+        public string NombreDocente { get; set; }
+
+    }
+}
